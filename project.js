0e94d9006d642e28017beb6a459a7350df4bdf26
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-07-03 23:28:07 +0530

    Create project.js

---
+++ project.js
@@ -0,0 +1,12 @@
+const deposit = () => {
+  while (true) {
+    const depositAmount = prompt("Enter a deposit amount: ");
+    const numberDepositAmount = parseFloat(depositAmount);
+
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+      console.log("Invalid deposit amount, try again.");
+    } else {
+      return numberDepositAmount;
+    }
+  }
+};
