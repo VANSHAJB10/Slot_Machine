9f99802949845b89ddf0aa463e0d88a3ad4d90e8
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-07-03 23:35:39 +0530

    Update project.js

--- project.js
+++ project.js
@@ -1,3 +1,12 @@
+// 1. Despot some money
+// 2. Determine the number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. check if the user won
+// 6. give the user their winnings
+// 7. play again
+
+
 const deposit = () => {
   while (true) {
     const depositAmount = prompt("Enter a deposit amount: ");
