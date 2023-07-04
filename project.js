78c73d0e3cdd4487ac48e9ff4f71736adb785dc4
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-07-04 23:03:23 +0530

    Update project.js

--- project.js
+++ project.js
@@ -19,3 +19,16 @@ const deposit = () => {
     }
   }
 };
+
+const getNumberOfLines = () => {
+  while (true) {                                                                 // loop keeps on repeating till user enters a valid amount
+    const lines = prompt("Enter the number of lines to bet on (1-3): ");                               // asking user by printing this
+    const numberOfLines = parseFloat(lines);
+
+    if (isNaN(numberOfLines) || numberOfLines <= 0) {
+      console.log("Invalid input, try again.");
+    } else {
+      return numberOfLines;                                                // once valid amount is entered--> the value us returned and the loop ends
+    }
+  }
+};
