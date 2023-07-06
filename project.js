1c186157817cb1c6099b2f45609f608e54e5b960
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-07-06 23:42:30 +0530

    Update project.js

--- project.js
+++ project.js
@@ -53,6 +53,20 @@ const spin = () => {
       symbols.push(symbol);
     }
   }
+   const reels = [];
+  for (let i = 0; i < COLS; i++) {
+    reels.push([]);
+    const reelSymbols = [...symbols];
+    for (let j = 0; j < ROWS; j++) {
+      const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+      const selectedSymbol = reelSymbols[randomIndex];
+      reels[i].push(selectedSymbol);
+      reelSymbols.splice(randomIndex, 1);
+    }
+  }
+
+  return reels;
+};
 
 
 
